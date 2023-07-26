cd54695955c1fce19953a6ad6bdebe84964afc83
Piyush <piyush6866@gmail.com>
2023-07-26 12:17:25 +0530

    new room creation initial logic

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,28 +1,62 @@
 const express = require('express');
-const cors = require('cors'); // Import the cors middleware
+const cors = require('cors');
 const app = express();
 
-// Use the cors middleware to allow requests from any origin
-// Create the http server and pass it to socket.io
 const httpServer = require('http').createServer(app);
 const io = require('socket.io')(httpServer);
 
-const users = {};
+const MAX_PLAYERS_PER_ROOM = 2; // Maximum players allowed per room
+const rooms = {}; // Object to store rooms and their players
+const users = {}; // Object to store the users
+
 io.on('connection', socket => {
-    socket.on('new-user-joined', name =>{
-        users[socket.id] = name; 
-        socket.broadcast.emit('user-joined', name);
-    });
-    socket.on('send', message =>{
-        socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
-    });
-    socket.on('disconnect', message=>{
-        socket.broadcast.emit('left', users[socket.id]);
-        delete users[socket.id];
-    });
+  // Function to get the available room with space for one more player
+  const getAvailableRoom = () => {
+    for (const room in rooms) {
+      if (rooms[room].length < MAX_PLAYERS_PER_ROOM) {
+        return room;
+      }
+    }
+    return null;
+  };
+
+  socket.on('new-user-joined', name => {
+    const room = getAvailableRoom();
+    if (!room) {
+      // Create a new room if no available rooms with space are found
+      const newRoom = `room${Date.now()}`;
+      rooms[newRoom] = [socket.id];
+      socket.join(newRoom);
+      socket.emit('room-created', newRoom);
+    } else {
+      rooms[room].push(socket.id);
+      socket.join(room);
+    }
+
+    io.to(room).emit('user-joined', name);
+  });
+
+  socket.on('send', message => {
+    const room = Object.keys(socket.rooms).find(room => room !== socket.id);
+    io.to(room).emit('receive', { message: message, name: users[socket.id] });
+  });
+
+  socket.on('disconnect', () => {
+    for (const room in rooms) {
+      const index = rooms[room].indexOf(socket.id);
+      if (index !== -1) {
+        rooms[room].splice(index, 1);
+        io.to(room).emit('left', users[socket.id]);
+        if (rooms[room].length === 0) {
+          delete rooms[room]; // Remove the room if no players are left
+        }
+        break;
+      }
+    }
+  });
 });
 
 const PORT = 8000;
 httpServer.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
