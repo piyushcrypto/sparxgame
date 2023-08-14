702e425e2e5620229611551d79c2b8da31080d87
Piyush <piyush6866@gmail.com>
2023-08-14 17:49:55 +0530

    adding the game

--- js/client.js
+++ js/client.js
@@ -1,34 +1,125 @@
- const socket = io('http://localhost:8000');
- const form = document.getElementById('send-container');
- const messageInput =document.getElementById('messageInp');
- const messageContainer = document.querySelector(".messagecontainer");
- const answerContainer = document.getElementById('answerInp')
- //const audio = new Audio('ting.mp3');
- const append = (message, position)=>{
-   const messageElement = document.createElement('div');
-   messageElement.innerText = message;
-   messageElement.classList.add('message');
-   messageElement.classList.add(position);
-   messageContainer.append(messageElement);
+const socket = io('http://localhost:8000');
+const messageform = document.getElementById('send-container');
+const answerform = document.getElementById('answer-container');
+const messageInput = document.getElementById('messageInp');
+const answerInput = document.getElementById('answerInp'); // Assume an input box for answers
+const messageContainer = document.querySelector(".messagecontainer");
+const questionContainer = document.querySelector(".questioncontainer");
+const timerContainer = document.querySelector('.timercontainer');
+const hintContainer = document.querySelector('.hintcontainer'); // Assume a display element for the timer
+const MAX_ROUNDS_PER_GAME = 10;
+const INITIAL_ROUND_TIME = 30000; // 30 seconds
+const TIME_DECREMENT_PER_ROUND = 1000;
+let countdown = 0;
+let currentRoundTime = INITIAL_ROUND_TIME; // Initialize round time
+
+const append = (message, position = 'center') => {
+  const messageElement = document.createElement('div');
+  messageElement.innerText = message;
+  messageElement.classList.add('message');
+  messageElement.classList.add(position);
+  messageContainer.append(messageElement);
 };
 
-const Username = prompt("Enter your Username for Joining");
- socket.emit('new-user-joined', Username);
- socket.on('user-joined', name=>{
-    append(`${name} joined.`, 'left');
- })
- socket.on('receive', data =>{
-   append(`${data.name}: ${data.message}`, 'right');
-})
+const questionappend = (question, position = 'center') => {
+  const questionElement = document.createElement('div');
+  questionElement.innerText = question;
+  questionElement.classList.add('question');
+  questionContainer.append(questionElement);
+}
 
-socket.on('room-created', newRoom =>{
-  append(`Room ${newRoom} created. Waiting for second player to join.`)
-})
+const timerappend = (time, position = 'center') => {
+  const timerElement = document.createElement('div');
+  timerElement.innerText = time;
+  timerElement.classList.add('time');
+  timerContainer.append(timerElement);
+}
 
-form.addEventListener('submit',(e)=>{
-   e.preventDefault();
-   const message = messageInput.value ;
-   append(`You: ${message}`, 'left');
-   socket.emit('send', message);
-   messageInput.value = '';
+const hintappend = (hint, position = 'center') => {
+  const hintElement = document.createElement('div');
+  hintElement.innerText = hint;
+  hintElement.classList.add('hint');
+  hintContainer.append(hintElement);
+}
+
+let Username = prompt("Enter your Username for Joining");
+
+// Keep prompting the user until a non-empty, non-null value is entered
+while (!Username || Username.trim() === '') {
+  Username = prompt("Username cannot be empty. Enter your Username for Joining");
+}
+
+socket.emit('new-user-joined', Username);
+
+socket.on('user-joined', name => {
+  append(`${name} joined.`, 'left');
 });
+
+socket.on('receive', data => {
+  append(`${data.name}: ${data.message}`, 'right');
+});
+
+socket.on('room-created', newRoom => {
+  append(`Room ${newRoom} created. Waiting for second player to join.`);
+});
+
+socket.on('clear-countdown', ()=>{
+  countdown = 0;
+  timerContainer.innerHTML = '';
+})
+socket.on('start-round', data => {
+  questionContainer.innerHTML = ''; 
+  hintContainer.innerHTML = '';
+  questionappend(`New round started! Unscramble the word: ${data.shuffledWord}`);
+  hintappend(`Hint: Meaning : ${data.hint}`)
+  answerInput.value = ''; // Clear the answer input box
+  // Start the countdown timer
+  let timeRemaining = currentRoundTime;
+  timerappend(timeRemaining/1000);
+  countdown = setInterval(() => {
+    timerContainer.innerHTML = '';
+    timeRemaining -= 1000; // Decrease by 1 second
+    timerappend(timeRemaining/1000);
+    if (timeRemaining <= 0) {
+      clearInterval(countdown);
+      timerContainer.innerHTML = '';
+    }
+  }, 1000);
+
+  currentRoundTime -= TIME_DECREMENT_PER_ROUND; // Decrease the time for the next round
+});
+
+socket.on('round-winner', data => {
+  questionContainer.innerHTML = ''; 
+  questionappend(`${data.name} won the round!`);
+});
+
+socket.on('round-timeout', originalWord => {
+  questionContainer.innerHTML = ''; 
+  questionappend(`Round Timed Out. The correct answer is ${originalWord}!`);
+});
+
+socket.on('end-game', data => {
+  append(`Game over! The winner is ${data.winner}!`);
+});
+
+messageform.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  append(`You: ${message}`, 'left');
+  socket.emit('send', message);
+  messageInput.value = '';
+});
+
+answerform.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const answer = answerInput.value;
+  socket.emit('player-answer', answer);
+  answerInput.value = '';
+});
+
+// Handle submission of answers
+// answerInput.addEventListener('change', (e) => {
+//   const answer = answerInput.value;
+//   socket.emit('player-answer', answer);
+// });
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,89 +1,168 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-
+const axios = require('axios');
 const httpServer = require('http').createServer(app);
 const io = require('socket.io')(httpServer);
 
 const MAX_PLAYERS_PER_ROOM = 2; // Maximum players allowed per room
-const MAX_ROUNDS_PER_GAME = 5;
+const MAX_ROUNDS_PER_GAME = 10;
+const WORDS_PER_ROOM = 10;
+const INITIAL_ROUND_TIME = 30000; // 30 seconds
+const TIME_DECREMENT_PER_ROUND = 1000;
+
 const rooms = {}; // Object to store rooms and their players
 const users = {}; // Object to store the users
 
+const getWords = () => {
+  const data = fs.readFileSync('words.json', 'utf8');
+  return JSON.parse(data);
+};
+
+const shuffleWord = (word) => {
+  const letters = word.split('');
+  const shuffledLetters = letters.sort(() => 0.5 - Math.random());
+  return shuffledLetters.join('');
+};
+
+const getRandomWords = (words) => {
+  const shuffledWords = words.sort(() => 0.5 - Math.random()).slice(0, WORDS_PER_ROOM);
+  return shuffledWords.map(word => ({original: word, shuffled: shuffleWord(word) }));
+};
+
+const fetchWordDetails = async (word) => {
+  try {
+    const response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+    return response.data;
+  } catch (error) {
+    return 'Ouch !!! No hint available for the selected word!!!';
+  }
+};
 io.on('connection', socket => {
-    const readWordsFromFile = () => {
-        try {
-            const data = fs.readFileSync('words.json', 'utf8');
-            const wordsData = JSON.parse(data);
-            return wordsData.words;
-        } catch (err) {
-            console.error('Error reading words from file:', err);
-            return [];
-        }
-    };
 
-    // Function to select 10 random words
-    const getRandomWords = (wordsList, count) => {
-        const shuffled = wordsList.sort(() => 0.5 - Math.random());
-        return shuffled.slice(0, count);
-    };
-    // Function to get the available room with space for one more player
-    const getAvailableRoom = () => {
-        for (const room in rooms) {
-            if (rooms[room].length < MAX_PLAYERS_PER_ROOM) {
-                return room;
-            }
-        }
-        return null;
-    };
+  const getAvailableRoom = () => {
+    for (const room in rooms) {
+      if (rooms[room].players && rooms[room].players.length < MAX_PLAYERS_PER_ROOM) {
+        return room;
+      }
+    }
+    return null;
+  };
 
-    //shuffle the words and store key value pair
+  socket.on('new-user-joined', name => {
+    const room = getAvailableRoom();
+    if (!room) {
+        const newRoom = `room${Date.now()}`;
+        rooms[newRoom] = { players: [socket.id], words: [], timer: null };
+        socket.join(newRoom);
+        socket.emit('room-created', newRoom);
+        io.to(newRoom).emit('user-joined', name);
+    } else {
+        rooms[room].players.push(socket.id);
+        socket.join(room);
+        io.to(room).emit('user-joined', name);      
+    }
+    users[socket.id] = { name: name, score: 0 };
+    if(room){
+        if(rooms[room].players.length == MAX_PLAYERS_PER_ROOM){
+            const words = getWords();
+            const randomWordsWithShuffled = getRandomWords(words);
+            rooms[room].words = randomWordsWithShuffled;
+            rooms[room].currentRound = 0;
+            rooms[room].currentRoundTime = INITIAL_ROUND_TIME;
+            setTimeout(() => {
+            //initialize the round start
+                startRound(room);
+            }, 5000);
+        } 
+    } 
+  });
 
+  socket.on('send', message => {
+    const roomsArray = Array.from(socket.rooms);
+    const room = roomsArray.find(roomName => roomName !== socket.id);
+    socket.broadcast.to(room).emit('receive', { message: message, name: users[socket.id].name });
+  });
 
+  socket.on('player-answer', answer => {
+    const roomsArray = Array.from(socket.rooms);
+    const room = roomsArray.find(roomName => roomName !== socket.id);
+    const currentRound = rooms[room].currentRound;
+    const correctWord = rooms[room].words[currentRound-1].original;
 
-    socket.on('new-user-joined', name => {
-        const room = getAvailableRoom();
-        if (!room) {
-            //Selecting 10 random words for gameplay
-            const wordsList = readWordsFromFile();
-            const randomWords = getRandomWords(wordsList, 5);
-            const wordPairs = {};
-            // Create a new room if no available rooms with space are found
-            const newRoom = `room${Date.now()}`;
-            rooms[newRoom] = [socket.id];
-            socket.join(newRoom);
-            socket.emit('room-created', newRoom);
-            io.to(newRoom).emit('user-joined', name);
-        } else {
-            rooms[room].push(socket.id);
-            socket.join(room);
-        }
-        users[socket.id] = name;
-        io.to(room).emit('user-joined', name);
-    });
+    if (answer === correctWord && !rooms[room].roundWinner) {
+      users[socket.id].score += 1;
+      rooms[room].roundWinner = true;
+      io.to(room).emit('clear-countdown');
+      io.to(room).emit('round-winner', { name: users[socket.id].name });
+      setTimeout(() => startRound(room), 2000);
+    }
+  });
 
-    socket.on('send', message => {
-        const roomsArray = Array.from(socket.rooms);
-        const room = roomsArray.find(roomName => roomName !== socket.id);
-        socket.broadcast.to(room).emit('receive', { message: message, name: users[socket.id] });
-    });
-
-    socket.on('disconnect', () => {
-        for (const room in rooms) {
-            const index = rooms[room].indexOf(socket.id);
-            if (index !== -1) {
-                rooms[room].splice(index, 1);
-                io.to(room).emit('left', users[socket.id]);
-                if (rooms[room].length === 0) {
-                    delete rooms[room]; // Remove the room if no players are left
-                }
-                break;
-            }
-        }
-    });
+  socket.on('disconnect', () => {
+    for (const room in rooms) {
+      const index = rooms[room].players.indexOf(socket.id);
+      if (index !== -1) {
+        rooms[room].players.splice(index, 1);
+        io.to(room).emit('room-closed', users[socket.id]); // Inform remaining players that the room has been closed
+        delete rooms[room]; // Delete the room
+        break;
+      }
+    }
+  });
+  
 });
+
+const startRound = async (room) => {
+  if (!rooms[room]) {
+    // Room does not exist, so exit the function
+    return;
+  }
+
+  if (rooms[room].currentRound >= MAX_ROUNDS_PER_GAME) {
+    endGame(room);
+    return;
+  }
+
+  const originalWord = rooms[room].words[rooms[room].currentRound].original;
+  const wordDetails = await fetchWordDetails(originalWord);
+  const meaning = JSON.stringify(wordDetails[0]["meanings"][0]["definitions"][0]["definition"]);
+
+  rooms[room].roundWinner = false; // Initialize roundWinner to false
+  const shuffledWord = rooms[room].words[rooms[room].currentRound].shuffled;
+  io.to(room).emit('start-round', {shuffledWord: shuffledWord, hint: meaning});
+
+  // Store the reference to the timer in the room object
+  rooms[room].timer = setTimeout(() => {
+    if (!rooms[room] || !rooms[room].roundWinner) { // Check if rooms[room] exists
+      if (rooms[room]) {
+        io.to(room).emit('round-timeout', originalWord); // Emit a timeout event only if room exists
+        io.to(room).emit('clear-countdown');
+      }
+      startRound(room);
+    }
+  }, rooms[room].currentRoundTime);
+
+  rooms[room].currentRoundTime -= TIME_DECREMENT_PER_ROUND;
+  rooms[room].currentRound += 1;
+};
+
+
+const endGame = (room) => {
+  let winner; // Declare winner variable here
+  const scores = rooms[room].players.map(playerId => users[playerId].score);
+  if (scores[0] > scores[1]) {
+    winner = users[rooms[room].players[0]].name;
+  } else if (scores[0] < scores[1]) {
+    winner = users[rooms[room].players[1]].name;
+  } else {
+    winner = "No One";
+  }
+  io.to(room).emit('end-game', { winner: winner });
+};
+
 
 const PORT = 8000;
 httpServer.listen(PORT, () => {
-    console.log(`Server is up and running.....`);
+  console.log(`Server is up and running.....`);
 });
